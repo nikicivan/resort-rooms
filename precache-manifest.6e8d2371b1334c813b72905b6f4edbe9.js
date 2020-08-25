c03003b614e8eaec8447b546fce301b519ee60f0
unknown <ivan.nikic@gmail.com>
2020-08-25 13:07:41 +0200

    Updates

---
+++ precache-manifest.6e8d2371b1334c813b72905b6f4edbe9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9d0d33193831512b34f9bbcfd109a9",
+    "url": "/resort-rooms/index.html"
+  },
+  {
+    "revision": "9b4f1a2a70e8a2990446",
+    "url": "/resort-rooms/static/css/main.edb641ed.chunk.css"
+  },
+  {
+    "revision": "4e3c9f00b10c1330d94b",
+    "url": "/resort-rooms/static/js/2.64fec3a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/resort-rooms/static/js/2.64fec3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4f1a2a70e8a2990446",
+    "url": "/resort-rooms/static/js/main.868a346d.chunk.js"
+  },
+  {
+    "revision": "563e8b2bc3983d39dd45",
+    "url": "/resort-rooms/static/js/runtime-main.52ee511a.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/resort-rooms/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/resort-rooms/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "c7c76c7a194983fa489c0e1b2d48dd3b",
+    "url": "/resort-rooms/static/media/logo.c7c76c7a.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/resort-rooms/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/resort-rooms/static/media/room-2.052c7681.jpeg"
+  }
+]);
